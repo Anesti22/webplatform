3bfdd025969d574981d6a640d68449b6d6163eeb
Anesti Gjikoka <anesti.gjikoka@gmail.com>
2019-02-08 14:40:14 +0100

    Update the '2- move_mouse'

---
+++ 1- Pointer Events/app.js
@@ -0,0 +1,10 @@
+// // animationName
+// // The value of the animation-name property of the animation that fired the animation event.
+// // Note: This property is read-only.
+
+// var result = element.animationName;
+
+// //define animation event
+// var myAnimEvent = new AnimationEvent();
+// //retrieve name
+// var myAnimName = myAnimEvent.animationName;
